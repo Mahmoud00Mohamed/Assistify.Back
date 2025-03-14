e4dbaa3c3a6d54ded7f345a8dd1b25335ca367f5
Mahmoud <mahmoud@gmail.com>
2025-03-14 06:31:11 +0200

    last14

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,17 +13,19 @@ import {
   checkUsername,
 } from "../controllers/authController.js";
 import passport from "../config/auth.js";
+import { OAuth2Client } from "google-auth-library"; // أضف هذا السطر
 
 const router = express.Router();
 
-// إعداد معدل التحديد للمسارات الحساسة (5 محاولات خلال 15 دقيقة)
+// إعداد معدل التحديد للمسارات الحساسة
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 دقيقة
-  max: 5000, // عدد المحاولات القصوى
+  windowMs: 15 * 60 * 1000,
+  max: 5000,
   message: { error: "Too many requests. Please try again later." },
   standardHeaders: true,
   legacyHeaders: false,
 });
+
 // تطبيق الـ Rate Limiting على المسارات الحساسة
 router.post("/signup", authLimiter, signup);
 router.post("/login", authLimiter, login);
@@ -37,6 +39,7 @@ router.post("/reset-password", resetPassword);
 router.post("/refresh-token", refreshAccessToken);
 router.post("/logout", logout);
 
+// مسار جديد للتعامل مع طلب POST من Google Sign-In
 router.post("/google", async (req, res) => {
   const { token } = req.body;
 
@@ -45,8 +48,6 @@ router.post("/google", async (req, res) => {
   }
 
   try {
-    // التحقق من رمز Google Identity Token باستخدام مكتبة google-auth-library
-    const { OAuth2Client } = require("google-auth-library");
     const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
     const ticket = await client.verifyIdToken({
       idToken: token,
@@ -54,7 +55,6 @@ router.post("/google", async (req, res) => {
     });
     const payload = ticket.getPayload();
 
-    // البحث عن المستخدم أو إنشاؤه
     let user = await User.findOne({ email: payload.email });
     if (!user) {
       user = new User({
@@ -82,4 +82,30 @@ router.post("/google", async (req, res) => {
     res.status(401).json({ message: "Invalid Google token." });
   }
 });
+
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    const accessToken = generateAccessToken(req.user._id);
+    const refreshToken = generateRefreshToken(req.user._id);
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    });
+
+    res
+      .status(200)
+      .json({ accessToken, message: "✅ Logged in with Google successfully!" });
+  }
+);
+
 export default router;
