53db3bfd6f5e801d7128356cd04f7623932674b1
Mahmoud <mahmoud@gmail.com>
2025-03-19 06:48:26 +0200

    finish today

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3002/api/auth/google/callback",
+      callbackURL: "https://assistify-back.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
