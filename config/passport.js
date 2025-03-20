053f8a8a371a37b50ccbe469c99df76fc81c35ef
Mahmoud <mahmoud@gmail.com>
2025-03-21 01:03:09 +0200

    add api

--- config/passport.js
+++ config/passport.js
@@ -13,7 +13,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://assistify-back.onrender.com/api/auth/google/callback",
+      callbackURL: "https://api.assistify.site/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
